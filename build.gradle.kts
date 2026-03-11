36b183a36e0e06b3023eb795cffd7c06fbe48971
gr1zDEV <gr1zztv@outlook.com>
2026-03-11 11:23:55 -0400

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,100 @@
+plugins {
+    `java-library`
+    alias(libs.plugins.paperweight.userdev)
+    alias(libs.plugins.run.paper) // Adds runServer and runMojangMappedServer tasks for testing
+
+    // Shades and relocates dependencies into our plugin jar. See https://imperceptiblethoughts.com/shadow/introduction/
+    alias(libs.plugins.shadow)
+}
+
+group = "com.moulberry.axiom"
+version = "5.0.3+1.21.11"
+description = "Serverside component for Axiom on Paper"
+
+java {
+    // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 11 installed for example.
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+repositories {
+    maven("https://repo.viaversion.com") {
+        content {
+            includeGroup("com.viaversion")
+        }
+    }
+    maven("https://maven.enginehub.org/repo/") { // WorldGuard
+        content {
+            includeGroup("com.sk89q.worldguard")
+            includeGroup("com.sk89q.worldedit")
+            includeGroup("com.sk89q.worldguard.worldguard-libs")
+            includeGroup("com.sk89q.worldedit.worldedit-libs")
+        }
+    }
+    maven("https://maven.playpro.com") { // CoreProtect
+        content {
+            includeGroup("net.coreprotect")
+        }
+    }
+    mavenCentral()
+}
+
+dependencies {
+    paperweight.paperDevBundle("1.21.11-R0.1-SNAPSHOT")
+    implementation(libs.reflection.remapper)
+    implementation(libs.cloud.paper)
+
+    // Zstd Compression Library
+    implementation(libs.zstd.jni)
+
+    // LuckPerms event integration
+    compileOnly(libs.luckperms)
+
+    // ViaVersion support
+    compileOnly(libs.viaversion.api)
+
+    // WorldGuard support
+    compileOnly(libs.worldguard.bukkit)
+
+    // PlotSquared support
+    implementation(platform(libs.bom.newest))
+    compileOnly(libs.plotsquared.core)
+    compileOnly(libs.plotsquared.bukkit) { isTransitive = false }
+
+    // CoreProtect support
+    compileOnly(libs.coreprotect)
+}
+
+tasks {
+    assemble {
+        dependsOn(shadowJar)
+    }
+    compileJava {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+
+        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
+        // See https://openjdk.java.net/jeps/247 for more information.
+        options.release.set(21)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+        val props = mapOf(
+                "name" to project.name,
+                "version" to project.version,
+                "description" to project.description,
+                "apiVersion" to "1.21"
+        )
+        inputs.properties(props)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+
+    shadowJar {
+        // helper function to relocate a package into our package
+        fun reloc(pkg: String) = relocate(pkg, "com.moulberry.axiom.dependency.$pkg")
+        reloc("xyz.jpenilla:reflection-remapper")
+    }
+}
